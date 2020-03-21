24d277cdeca32f02140ca253fe2d858360b71c51
Paul AYling <jimmyyukka@gmail.com>
2020-03-21 09:34:39 +0000

    dev: table-two-column

--- src/components/molecules/table-two-column.js
+++ src/components/molecules/table-two-column.js
@@ -1,24 +1,24 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
-export default function(props) {
-  const sectionNumber = props.sectionNumber;
-  const tableData = props.data[sectionNumber].tableData;
-
-
-  let tableRender = tableData.map((item, key) => {
-    return(
-          <li>{item.c1}</li>
-
-          
-
-     
-
-    )
-
-  });
-console.log(tableRender)
-  return <Fragment>
-
-    
-  </Fragment>;
+class TableTwoColumn extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const sectionNumber = this.props.sectionNumber;
+    const currentData = this.props.data[sectionNumber]
+    const elementsRender = currentData.tableData.map(elements => {
+      return (
+        <div className='container-flex-row border-lightgray'>
+          <div className={'table-side-header gray container-flex-grow-'+this.currentData.tableStyleCol1}>
+          {elements.c1}
+          </div>
+          <div className={'container-flex-grow-' + this.props.c2}></div>
+        </div>
+      );
+    });
+    return <h1>{elementsRender}</h1>;
+  }
 }
+
+export default TableTwoColumn;
